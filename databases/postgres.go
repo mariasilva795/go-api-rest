090ce500c0b3f642b5f7ed92f1d003a2b5da58bb
Maria Silva <maria_silva@Marias-MacBook-Air.local>
2024-09-18 08:52:22 -0500

    Implementacion de Postgress

---
+++ databases/postgres.go
@@ -0,0 +1,46 @@
+package databases
+
+import (
+	"context"
+	"database/sql"
+	"log"
+
+	"github.com/mariasilva795/go-api-rest/models"
+)
+
+type PostgresRepository struct {
+	db *sql.DB
+}
+
+func NewPostgresRepository(url string) (*PostgresRepository, error) {
+	db, err := sql.Open("postgress", url)
+	if err != nil {
+		return nil, err
+	}
+	return &PostgresRepository{db}, nil
+}
+
+func (repo *PostgresRepository) InsertUser(ctx context.Context, id int64) (*models.User, error) {
+	rows, err := repo.db.QueryContext(ctx, "SELECT id, email FROM users WHERE id = $1", id)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		err = rows.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	var user = models.User{}
+	for rows.Next() {
+		if err = rows.Scan(&user.Id, &user.Email); err == nil {
+			return &user, nil
+		}
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
